705540583217c1fa2764052b7b2ac9f216e83ea4
zira <66490318+triqa@users.noreply.github.com>
2025-12-27 18:09:39 +0000

    Make random generation of rock paper or scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+function getComputerChoice() {
+  // Randomly return either: rock, papers, or scissors
+
+  // Get a num either: 0 (rock), 1 (paper), 2 (scissors)
+  randNum = Math.floor(Math.random() * 3);
+
+  switch (randNum) {
+    case 0:
+      result = "rock";
+      break;
+    case 1:
+      result = "paper";
+      break;
+    case 2:
+      result = "scissors";
+      break;
+  }
+
+  console.log(`${randNum}: ${result}`);
+}
+
+getComputerChoice();
